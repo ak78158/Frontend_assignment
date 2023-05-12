9a21ebe84139d2e18dc3911f2e89284048a37ba2
Amit <amit.k@goodworker.in>
2023-05-12 20:06:03 +0530

    initial 4th commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { useState } from "react";
+import LoginEmail from "./pages/loginEmail/loginEmail";
 import RequestOtp from "./pages/requestOtp/requestOtp.";
 
 function App() {
   const [emailCompleted, setEmailComplete] = useState(false);
-
+  const onEmailComplete = () => {
+    setEmailComplete(true);
+  };
   return (
     <div>
       {emailCompleted ? (
         <RequestOtp />
       ) : (
-        // <LoginEmail onEmailComplete={onEmailComplete} />
-        <RequestOtp />
+        <LoginEmail onEmailComplete={onEmailComplete} />
       )}
     </div>
   );
